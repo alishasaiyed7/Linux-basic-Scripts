e3900644d9baf3e0651c50efce34de0a2dc2d17d
ALISHA SAIYED <106082357+alishasaiyed7@users.noreply.github.com>
2025-10-15 19:56:18 +0530

    PackageInstallation.sh commit

--- Prj - 6 Package Management & Custom Repo/PackageInstallation.sh
+++ Prj - 6 Package Management & Custom Repo/PackageInstallation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+## used for Ubuntu, Debian ,Linux Mint, Kali Linux
 PACKAGES=("vim" "git" "wget") //as per your requirement you can add
 for pkg in "${PACKAGES[@]}"; do #this is a for loop so it will check for all 3 one by 1 and then it will exit 
     echo -n "🔍 Checking $pkg... " 
@@ -8,6 +10,20 @@ for pkg in "${PACKAGES[@]}"; do #this is a for loop so it will check for all 3 o
     else #if not
         echo "Not installed ❌ — Installing..."
         sudo apt install -y $pkg #it will install the pacakge
+    fi
+done
+
+
+## used for CentOS (before version 8), Red Hat Enterprise Linux (RHEL), Amazon Linux 1 & 2
+
+PACKAGES=("vim" "git" "wget") //as per your requirement you can add
+for pkg in "${PACKAGES[@]}"; do #this is a for loop so it will check for all 3 one by 1 and then it will exit 
+    echo -n "🔍 Checking $pkg... " 
+    if command -v $pkg &>/dev/null; then # here we are checking if -v vim (version of vim) gives any output then
+        echo "Already installed ✅" # that its already installed
+    else #if not
+        echo "Not installed ❌ — Installing..."
+        sudo yum install -y $pkg #it will install the pacakge
     fi
 done
 
